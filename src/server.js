0778d8232612c8b9ab98800bf8d4067fe79615a1
Nadezhda Vazharova <Nazdezhda_Vazharova@epam.com>
2018-04-24 11:02:18 +0300

    Task 2

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ var express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + './../../')); //serves the index.html
+app.use(express.static(__dirname)); //serves the index.html
 app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,70 @@
 
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+const autoprefixer = require("autoprefixer");
 
-var BUILD_DIR = path.resolve(__dirname, './build');
-var APP_DIR = path.resolve(__dirname, './src/client');
+const BUILD_DIR = path.resolve(__dirname, "./build");
+const APP_DIR = path.resolve(__dirname, "./src");
 
 const config = {
-   entry: {
-     main: APP_DIR + '/index.js'
-   },
-   output: {
-     filename: 'bundle.js',
-     path: BUILD_DIR,
-   },
-   module: {
-    rules: [     
-     {
-       test: /\.(jsx|js)?$/,
-       use: [{
-         loader: "babel-loader",
-         options: {
-           cacheDirectory: true,
-           presets: ['react', 'env'] 
-         }
-       }]
-     }
-    ],
-
-  }
+  mode: "development",
+  entry: APP_DIR + "/index.js",
+  devtool: "cheap-module-eval-source-map",
+  output: {
+    filename: "bundle.js",
+    path: BUILD_DIR,
+    publicPath: ""
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        //order is important, start reading from bottom
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]__[local]__[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => {
+                autoprefixer({
+                  "browsers": [
+                    "> 1%",
+                    "last 2 versions"
+                  ]
+                });
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
 };
 
 module.exports = config;
